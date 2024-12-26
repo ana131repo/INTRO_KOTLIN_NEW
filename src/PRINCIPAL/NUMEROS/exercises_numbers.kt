b5395d2beb61965c4522b76c3b25db8d771c0581
ana131 <antakarana131@gmail.com>
2024-12-26 10:52:55 +0100

    numbers_1

---
+++ src/PRINCIPAL/NUMEROS/exercises_numbers.kt
@@ -0,0 +1,21 @@
+package PRINCIPAL.NUMEROS
+
+/*Ejercicio 1
+Crea una función que dado 2 números enteros los sume y devuelva el resultado
+Ejemplo: 2+2 -> 4
+ */
+
+fun exercise1() {
+    println("Sumar dos números enteros ")
+    var resultado: Int = 0
+    println("Primer número")
+    val number1 = readlnOrNull()?.toIntOrNull() ?: 0
+    println("Segundo número")
+    val number2 = readlnOrNull()?.toIntOrNull() ?: 0
+    resultado = suma(number1, number2)
+    println("Resultado $resultado")
+}
+
+fun suma(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
