5a135b7978172574147d7824312cd7f99a82ffd2
ana131 <antakarana131@gmail.com>
2024-12-26 10:54:45 +0100

    numbers_2

--- src/PRINCIPAL/NUMEROS/exercises_numbers.kt
+++ src/PRINCIPAL/NUMEROS/exercises_numbers.kt
@@ -19,3 +19,23 @@ fun exercise1() {
 fun suma(num1: Int, num2: Int): Int {
     return num1 + num2
 }
+
+/*Ejercicio 2
+Crea una función que dado 2 números enteros los multiplique y devuelva el resultado
+Ejemplo:2*2 -> 4
+*/
+
+fun exercise2() {
+    println("Multiplicar dos números enteros ")
+    println("Primer número")
+    val number1 = readlnOrNull()?.toIntOrNull() ?: 0
+    println("Segundo número")
+    val number2 = readlnOrNull()?.toIntOrNull() ?: 0
+    val resultado = multiplicar(number1, number2)
+    println("Resultado $resultado")
+}
+
+fun multiplicar(num1: Int, num2: Int): Int {
+    return num1 * num2
+
+}
